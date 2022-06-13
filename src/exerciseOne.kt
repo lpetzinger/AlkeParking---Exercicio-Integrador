4f106f668b83b0e76b18dc876f25ade91021ec18
Lucas Petzinger <lpetzinger@br0c02fq0rmmd6m.ml.com>
2022-06-13 17:46:20 -0300

    feat: exercise 4 - parkedTime

--- src/exerciseOne.kt
+++ src/exerciseOne.kt
@@ -1,6 +1,10 @@
 import java.util.*
 
-data class ParkingSpace(var vehicle: Vehicle)
+data class ParkingSpace(var vehicle: Vehicle){
+    val MINUTES_IN_MILISECONDS = 60000
+    val parkedTime: Long
+    get() = (Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis)/ MINUTES_IN_MILISECONDS
+}
 
 data class Parking(val vehicles: MutableSet<Vehicle> = mutableSetOf()) {
         fun setVehicles(value: Vehicle) = vehicles.add(value)
