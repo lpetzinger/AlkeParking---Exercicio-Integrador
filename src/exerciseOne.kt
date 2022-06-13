bb4f92f66db99a790f304ae9845a6baab21f7bf9
Oryange Strifezze <oryange.strifezze@mercadolivre.com>
2022-06-13 19:30:32 -0300

    feat: exercise 8

--- src/exerciseOne.kt
+++ src/exerciseOne.kt
@@ -2,34 +2,45 @@ import java.util.*
 
 data class ParkingSpace(var vehicle: Vehicle, val parking: Parking ){
     val MINUTES_IN_MILISECONDS = 60000
-    val parkedTime: Long
-        get() = (Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis)/ MINUTES_IN_MILISECONDS
+    val parkedTime: Int
+        get() = ((Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis)/ MINUTES_IN_MILISECONDS).toInt()
 
     fun checkoutVehicle(plate: String){
         if(vehicle.plate == plate){
             parking.vehicles.remove(vehicle)
-            onSuccess(10)
-        }else{
+            onSuccess(calculateFee(vehicle.type, parkedTime))
+        } else {
             onError()
         }
 
     }
-    fun onSuccess(fee: Int){
+   private fun onSuccess(fee: Int){
 
     }
 
-    fun onError(){
+    private fun onError(){
 
+    }
+
+    private fun calculateFee(type: VehicleType, parkedTime: Int) : Int {
+        var discountCard = 0
+        if(!vehicle.discountCard.isNullOrEmpty()) discountCard = 15/100
+
+        if(parkedTime <= 120 ) {
+            return type.fee - (type.fee*discountCard)
+        } else {
+            return ((parkedTime - 120) % 15) * 5 + type.fee - (type.fee * discountCard)
+        }
     }
 }
 
 data class Parking(val vehicles: MutableSet<Vehicle> = mutableSetOf()) {
         private val maxNumberOfVehicles = 20
         fun addVehicle (vehicle: Vehicle): Boolean {
-            if (vehicles.size < 20){
+            if(vehicles.size < 20){
                 println("Welcome to AlkeParking!")
                 return vehicles.add(vehicle)
-            }else{
+            } else{
                 println("Sorry, the check-in failed")
                 return false
             }
@@ -47,11 +58,11 @@ data class Vehicle(val plate: String, val type: VehicleType, val discountCard: S
     override fun hashCode(): Int = this.plate.hashCode()
 }
 
-enum class VehicleType(val type: String, fee: Int) {
-    CAR("Car", 20),
-    MOTORCYCLE("Motorcycle", 15),
-    MICROBUS("Micro Bus",25 ),
-    BUS("Bus", 30)
+enum class VehicleType(val type: String, val fee: Int) {
+    CAR(type="Car", fee = 20),
+    MOTORCYCLE(type="Motorcycle", fee = 15),
+    MICROBUS(type= "Micro Bus", fee = 25 ),
+    BUS(type = "Bus", fee = 30)
 }
 
 
